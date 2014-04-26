63ed2a35fabd9914aff22503a409681c7e3de949
snowleung <snowleung@gmail.com>
2014-04-26 12:17:47 +0800

    chessboard load function dev

--- jinziqi_chess.py
+++ jinziqi_chess.py
@@ -15,15 +15,22 @@ class Player():
 class ChessBoard():
     def __init__(self):
         self._chesses = []
-    def chesses(self):
-        return self._chesses
-
+    def load_chesses(self, chesses_info):
+        for c in chesses_info:
+            self._chesses.append(c)
+    def chesses(self, id = None):
+        if id == None:
+            return self._chesses
+        for c in self._chesses:
+            if c.id == id:
+                return c
+        return None
 
 class Chess(Player):
-    def __init__(self):
-        self.id = -1
-        self.x = 0
-        self.y = 0
+    def __init__(self, id = -1, x = 0, y = 0):
+        self.id = id
+        self.x = x
+        self.y = y
 
 class ChessBoardTest(unittest.TestCase):
     def setUp(self):
@@ -32,6 +39,23 @@ class ChessBoardTest(unittest.TestCase):
         chessboard = ChessBoard()
         self.assertTrue([] == chessboard.chesses())
         self.assertTrue(0 == len(chessboard.chesses()))
+    def testLoadChesses(self):
+        #jinziqi rule
+        _chessboard = {1:(0,0), 2:(0,1), 3:(0,2), 4:(1,0), 5:(1,1), 6:(1,2), 7:(2,0), 8:(2,1), 9:(2,2)}
+        _chessinfo = []
+        for k,v in _chessboard.items():
+            _chessinfo.append(Chess(k, v[0], v[1]))
+        self.chess_board.load_chesses(_chessinfo)
+        self.assertTrue((0,0) == self.chess_board.chesses(1).chess_XY)
+        self.assertTrue((0,1) == self.chess_board.chesses(2).chess_XY)
+        self.assertTrue((0,2) == self.chess_board.chesses(3).chess_XY)
+        self.assertTrue((1,0) == self.chess_board.chesses(4).chess_XY)
+        self.assertTrue((1,1) == self.chess_board.chesses(5).chess_XY)
+        self.assertTrue((1,2) == self.chess_board.chesses(6).chess_XY)
+        self.assertTrue((2,0) == self.chess_board.chesses(7).chess_XY)
+        self.assertTrue((2,1) == self.chess_board.chesses(8).chess_XY)
+        self.assertTrue((2,2) == self.chess_board.chesses(9).chess_XY)
+
 
 class ChessTest(unittest.TestCase):
     def setUp(self):
