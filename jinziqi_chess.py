9d765ed564030ed3756f954774fa5d3fd516abd5
snowleung <snowleung@gmail.com>
2014-04-27 16:53:25 +0800

    use position to instead of chessboard's  chess

--- jinziqi_chess.py
+++ jinziqi_chess.py
@@ -23,27 +23,21 @@ class Player():
 class ChessBoard():
     def __init__(self):
         self._chesses = []
-#     def load_chesses(self, chesses_info):
-#         for c in chesses_info:
-#             self._chesses.append(c)
+    def load_chesses(self, chesses_info):
+        self._chesses = []
+        for c in chesses_info:
+            self._chesses.append(c)
     def put_chess(self, chess):
-        if self.chesses(chess.id) == None:
-            self._chesses.append(chess)
-            return True
-        return False
-    def chesses(self, id = None):
-        if id == None:
-            return self._chesses
-        for c in self._chesses:
-            if c.id == id:
-                return c
-        return None
+        pass
+    def chesses(self):
+        return self._chesses
     
 class Chess(Player):
     def __init__(self, id = -1, x = 0, y = 0):
         self.id = id
         self.x = x
         self.y = y
+        self.is_use = False
     def chess_XY(self):
         return (self.x, self.y)
 
@@ -58,23 +52,22 @@ class ChessBoardTest(unittest.TestCase):
         chess1 = Chess(1, 1, 1)
         self.assertTrue(self.chess_board.put_chess(chess1))
         self.assertFalse(self.chess_board.put_chess(chess1))
-#     def testLoadChesses(self):
-#         #jinziqi rule
-#         _chessboard = {1:(0,0), 2:(0,1), 3:(0,2), 4:(1,0), 5:(1,1), 6:(1,2), 7:(2,0), 8:(2,1), 9:(2,2)}
-#         _chessinfo = []
-#         for k,v in _chessboard.items():
-#             _chessinfo.append(Chess(k, v[0], v[1]))
-#         self.chess_board.load_chesses(_chessinfo)
-#         self.assertTrue(1 == self.chess_board.chesses(1).id)
-#         self.assertTrue(2 == self.chess_board.chesses(2).id)
-#         self.assertTrue(3 == self.chess_board.chesses(3).id)
-#         self.assertTrue(4 == self.chess_board.chesses(4).id)
-#         self.assertTrue(5 == self.chess_board.chesses(5).id)
-#         self.assertTrue(6 == self.chess_board.chesses(6).id)
-#         self.assertTrue(7 == self.chess_board.chesses(7).id)
-#         self.assertTrue(8 == self.chess_board.chesses(8).id)
-#         self.assertTrue(9 == self.chess_board.chesses(9).id)
-
+    def testLoadChesses(self):
+        #jinziqi rule
+        _chessboard = {1:(0,0), 2:(0,1), 3:(0,2), 4:(1,0), 5:(1,1), 6:(1,2), 7:(2,0), 8:(2,1), 9:(2,2)}
+        _chessinfo = []
+        for k,v in _chessboard.items():
+            _chessinfo.append(Chess(k, v[0], v[1]))
+        self.chess_board.load_chesses(_chessinfo)
+        self.assertTrue(1 == self.chess_board.chesses(1).id)
+        self.assertTrue(2 == self.chess_board.chesses(2).id)
+        self.assertTrue(3 == self.chess_board.chesses(3).id)
+        self.assertTrue(4 == self.chess_board.chesses(4).id)
+        self.assertTrue(5 == self.chess_board.chesses(5).id)
+        self.assertTrue(6 == self.chess_board.chesses(6).id)
+        self.assertTrue(7 == self.chess_board.chesses(7).id)
+        self.assertTrue(8 == self.chess_board.chesses(8).id)
+        self.assertTrue(9 == self.chess_board.chesses(9).id)
 
 class ChessTest(unittest.TestCase):
     def setUp(self):
